f63261ce5d16f8519fcd96fa42b8a02400cb0a99
Kevin Doyle <kwdoyle1@gmail.com>
2025-09-25 15:04:44 -0400

    update plink path

--- merge_beds.sh
+++ merge_beds.sh
@@ -13,13 +13,14 @@ done
 
 # make full bed file
 echo Making full bed file
-../plink --merge-list mergeBED.list --make-bed --out $saveprefix
+# ../plink  # originally used plink binary from FastSparseGRM, but should be able to use any plink (in theory)
+plink --merge-list mergeBED.list --make-bed --out $saveprefix
 
 # prune bed file (make lists)
 echo Pruning bed file
-../plink  --bfile $saveprefix --indep-pairwise 50 5 0.1 --out ${saveprefix}.prunedlist
+plink  --bfile $saveprefix --indep-pairwise 50 5 0.1 --out ${saveprefix}.prunedlist
 
 # save pruned bed
 echo Saving pruned bed file
-../plink --bfile $saveprefix --extract ${saveprefix}.prunedlist.prune.in --make-bed --out ${saveprefix}_pruned
+plink --bfile $saveprefix --extract ${saveprefix}.prunedlist.prune.in --make-bed --out ${saveprefix}_pruned
 
