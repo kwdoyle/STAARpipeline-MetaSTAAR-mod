b68f70f1e1bedc6b872076be0417b7c697eb4e1e
Kevin Doyle <kwdoyle1@gmail.com>
2025-11-14 15:48:49 -0500

    MetaSTAAR scripts to run noncoding null model adjust on coding variants analysis

---
+++ MetaSTAAR/noncoding_MetaSTAARlite_per_gene.R
@@ -0,0 +1,71 @@
+library(STAAR)
+library(STAARpipeline)
+library(MetaSTAAR)
+library(MetaSTAARlite)
+
+## Directories of the study-specific summary statistics file folders
+file.dir <- c("/efs/garcia/users/kd2630/noncoding_telo/STAAR/MetaSTAAR_Discovery_TOPMed/Discovery/Gene_Centric_Noncoding_Analysis_cod_adj_null_mod/",
+              "/efs/garcia/users/kd2630/noncoding_telo/STAAR/MetaSTAAR_Discovery_TOPMed/TOPMed/Gene_Centric_Noncoding_Analysis_cod_adj_null_mod/")
+file.prefix <- c("10k_Cohort_rm_telo_qv_Gene_Noncoding_Analysis","TOPMed_Full_Cohort_grm_Gene_Noncoding_Analysis")
+## Sample sizes of participating studies
+sample.sizes <- c(3602, 6207)
+
+
+## variant_type
+variant_type <- "variant"
+## cov_maf_cutoff
+cov_maf_cutoff <- c(0.05, 0.05)
+
+## Use_annotation_weights
+Use_annotation_weights <- TRUE
+## Annotation name
+Annotation_name <- c("CADD","LINSIGHT","FATHMM.XF","aPC.EpigeneticActive","aPC.EpigeneticRepressed","aPC.EpigeneticTranscription",
+                     "aPC.Conservation","aPC.LocalDiversity","aPC.Mappability","aPC.TF","aPC.Protein")
+
+## output path
+output_path <- "/efs/garcia/users/kd2630/noncoding_telo/STAAR/MetaSTAAR_Discovery_TOPMed/Noncoding_Meta_Analysis_cod_adj_null_mod/"
+dir.create(output_path, recursive=T, showWarnings=F)
+## output file name
+output_file_name <- "noncodingMeta"
+
+
+results_noncoding <- c()
+
+sumstat.file.path <- paste0(file.dir, file.prefix, "_sumstat.Rdata")
+cov.file.path <- paste0(file.dir, file.prefix, "_cov.Rdata")
+noncoding_sumstat_list <- sapply(sumstat.file.path, function(x) mget(load(x)), simplify = TRUE)
+noncoding_cov_list <- sapply(cov.file.path, function(x) mget(load(x)), simplify = TRUE)
+
+# genes to analyze
+genes_to_analyze <- unique(unlist(lapply(noncoding_sumstat_list, names)))
+
+for (gn in genes_to_analyze) {
+	chr <- genes_info[genes_info$hgnc_symbol == gn, "chromosome_name"]
+	message("Gene: ", gn)
+	message("Chr: ", chr)
+
+	  noncoding_sumstat_gene_list <- lapply(sumstat.file.path, function(x) {
+	    noncoding_sumstat_list[[paste0(x,".noncoding_sumstat")]][[gn]]
+	  })
+	  noncoding_cov_gene_list <- lapply(cov.file.path, function(x) {
+	    noncoding_cov_list[[paste0(x,".noncoding_cov")]][[gn]]
+	  })
+
+	  results <- noncoding_MetaSTAARlite(chr=chr, gene_name=gn,
+					  sample.sizes=sample.sizes, noncoding_sumstat_gene_list=noncoding_sumstat_gene_list,
+					  noncoding_cov_gene_list=noncoding_cov_gene_list,
+					  cov_maf_cutoff=cov_maf_cutoff,
+					  rare_maf_cutoff=0.01, rv_num_cutoff=2,
+					  check_qc_label=TRUE, variant_type=variant_type,
+					  Use_annotation_weights=Use_annotation_weights, Annotation_name=Annotation_name)
+
+	# put into list with name of current gene
+	results_l <- list(results)
+	names(results_l) <- gn
+
+	results_noncoding <- append(results_noncoding, results_l)
+
+}
+
+save(results_noncoding, file=paste0(output_path, output_file_name, ".Rdata"))
+
---
+++ MetaSTAAR/noncoding_MetaSTAARlite_worker_per_gene.R
@@ -0,0 +1,101 @@
+library(gdsfmt)
+library(SeqArray)
+library(SeqVarTools)
+library(STAAR)
+library(STAARpipeline)
+library(MetaSTAAR)
+library(MetaSTAARlite)
+
+basedir <- commandArgs(TRUE)[1]
+resdir <- commandArgs(TRUE)[2]
+outdirnm <- commandArgs(TRUE)[3]
+afthresh <- as.numeric(commandArgs(TRUE)[4])
+variant_type <- commandArgs(TRUE)[5]
+#nullmodeldir <- commandArgs(TRUE)[6]
+# just hard code the null model dir in these scripts.
+nullmodeldir <- "/efs/garcia/users/kd2630/noncoding_telo/STAAR/MetaSTAAR_Discovery_TOPMed/coding_gene_vnt_null_models/"
+
+
+nullmodelfls <- list.files(nullmodeldir, pattern="*nullmodel.Rdata", full.names=T)
+# subset for current cohort 
+if (grepl("discovery", basename(resdir), ignore.case=T)) {
+	print("USING DISCOVERY")
+	nullmodelfls <- nullmodelfls[grep("discovery", basename(nullmodelfls), ignore.case=T)]
+} else if (grepl("topmed", basename(resdir), ignore.case=T)) {
+	print("USING TOPMED")
+	nullmodelfls <- nullmodelfls[grep("topmed", basename(nullmodelfls), ignore.case=T)]
+} else {
+	stop("Cohort could not be determined in order to select null models to use.")
+}
+
+
+hits <- read.csv("/efs/garcia/users/kd2630/noncoding_telo/STAAR/MetaSTAAR_Discovery_TOPMed/16_coding_hits_w_noncoding.csv")
+hits_use <- hits
+# only need gene and chr
+# Note: ALR2-SNX15 isn't tested here b/c specifically for SNX15, there is no corresponding coding and noncoding hit together.
+hits_use_tbl <- unique(hits_use[,c("Gene.name", "Chr")])
+
+
+agds_dir <- get(load(paste0(basedir, "/AssociationAnalysisPrestep/agds_dir.Rdata")))
+## QC_label
+QC_label <- "annotation/filter"
+## Annotation_dir
+Annotation_dir <- "annotation/info/FunctionalAnnotation"
+## Annotation channel
+Annotation_name_catalog <- get(load(paste0(basedir, "/AssociationAnalysisPrestep/Annotation_name_catalog.Rdata")))
+# Or equivalently
+# Annotation_name_catalog <- read.csv("/path_to_the_file/Annotation_name_catalog.csv")
+## Use_annotation_weights
+Use_annotation_weights <- TRUE
+## Annotation name
+Annotation_name <- c("CADD","LINSIGHT","FATHMM.XF","aPC.EpigeneticActive","aPC.EpigeneticRepressed","aPC.EpigeneticTranscription",
+                     "aPC.Conservation","aPC.LocalDiversity","aPC.Mappability","aPC.TF","aPC.Protein")
+
+## output path
+output_path <- paste0(resdir, "/", outdirnm)
+print(paste("Will create directory:", output_path))
+dir.create(output_path, recursive=TRUE)
+## output file name
+pthsplt <- unlist(strsplit(basedir, "\\/"))
+nmuse <- pthsplt[length(pthsplt)]
+output_file_name <- paste0(nmuse, "_Gene_Noncoding_Analysis")
+print(paste("Output file name is", output_file_name))
+
+
+# # Main function
+noncoding_sumstat <- list()
+noncoding_cov <- list()
+for (i in 1:nrow(hits_use_tbl)) {
+	chr <- hits_use_tbl$Chr[i]
+	gene_name <- hits_use_tbl$Gene.name[i]
+	print(paste(gene_name, "Chr:",  chr, sep=" "))
+	# genes_info is a built-in data frame from STAAR
+	#genes_info_chr <- genes_info[genes_info[,2]==chr,]
+	# can just subset for current gene instead
+	genes_info_gn <- genes_info[genes_info$hgnc_symbol == gene_name, ]
+
+	# get null model for current gene
+	null_model_gn <- grep(paste0("_", gene_name, "_"), nullmodelfls, value=T)
+	print(paste("Using null model:", null_model_gn))
+	obj_nullmodel <- get(load(paste0(null_model_gn))) 
+
+	## aGDS file
+	agds.path <- agds_dir[chr]
+	genofile <- seqOpen(agds.path)
+
+	results_temp <- noncoding_MetaSTAARlite_worker(chr=chr, gene_name=gene_name, genofile=genofile, obj_nullmodel=obj_nullmodel, 
+					      cov_maf_cutoff=0.05, signif.digits=NULL,
+					      QC_label=QC_label, check_qc_label=TRUE, variant_type=variant_type,
+					      Annotation_dir=Annotation_dir, Annotation_name_catalog=Annotation_name_catalog,
+					      Use_annotation_weights=Use_annotation_weights, Annotation_name=Annotation_name)
+
+	noncoding_sumstat[[gene_name]] <- results_temp$summary_stat_list
+	noncoding_cov[[gene_name]] <- results_temp$GTSinvG_rare_list
+
+	seqClose(genofile)
+}
+
+# I think I can save the entire output accross genes as a single file 
+save(noncoding_sumstat, file=paste0(output_path, output_file_name, "_sumstat", ".Rdata"), compress = "xz")
+save(noncoding_cov, file=paste0(output_path, output_file_name, "_cov", ".Rdata"), compress = "xz")
+
