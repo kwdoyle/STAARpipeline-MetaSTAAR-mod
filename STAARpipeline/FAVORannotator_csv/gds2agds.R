f7c71561aeb12eb0f29876ca043f39c9dc165624
Kevin Doyle <kwdoyle1@gmail.com>
2026-03-27 20:06:50 +0000

    ensure variant order matches between gds and anno file; run on ec2

--- STAARpipeline/FAVORannotator_csv/gds2agds.R
+++ STAARpipeline/FAVORannotator_csv/gds2agds.R
@@ -16,6 +16,7 @@ gds_file_name_1 <- commandArgs(TRUE)[3]
 gds_file_name_2 <- commandArgs(TRUE)[4]
 dir_anno <- commandArgs(TRUE)[5]
 
+message("aGDS for chr ", chr)
 
 ###########################################################################
 #           Main Function
@@ -29,6 +30,7 @@ library(readr)
 
 ### read annotation data
 # NOTE: read_csv reads in too many columns incorrectly as logical. Just use data.table::fread instead
+message("Reading ", paste0(dir_anno,"chr",chr,"/",anno_file_name_1,chr,anno_file_name_2))
 FunctionalAnnotation <- data.table::fread(paste0(dir_anno,"chr",chr,"/",anno_file_name_1,chr,anno_file_name_2))
 
 # set alphamissense to numeric, if it exists
@@ -67,21 +69,36 @@ gds.path <- paste0(dir_geno,gds_file_name_1,chr,gds_file_name_2)
 # so copy it to a tmp folder first, annotate it, then copy it back over
 
 # copy to tmp
-system(paste0("cp ", gds.path, " ~/tmp"))
-gds.path.local <- paste0("~/tmp/", basename(gds.path))
+system(paste0("cp ", gds.path, " /mnt/tmpstore/"))
+gds.path.local <- paste0("/mnt/tmpstore/", basename(gds.path))
 
 #genofile <- seqOpen(gds.path, readonly = FALSE)
 genofile <- seqOpen(gds.path.local, readonly = FALSE)
 
+# check that the variant order in FunctionalAnnotation matches that in the gds file.
+# if it doesn't, throw an error.
+pos = seqGetData(genofile, "position")
+refalt = seqGetData(genofile, "allele")
+refalt2 = gsub(",", "-", refalt)
+varid_gds = paste(chr, pos, refalt2, sep="-")
+# subset func anno data for the exact variants in the gds because, for some reason, there are still missmatches.
+#gds_vnt_match_idx <- which(FunctionalAnnotation$VarInfo %in% varid_gds)
+#FunctionalAnnotation_match <- FunctionalAnnotation[gds_vnt_match_idx, ]
+# need all matches of varid_gds in FuncAnno--even if it duplicates the FuncAnno row..
+gds_vnt_match_idx <- match(varid_gds, FunctionalAnnotation$VarInfo)
+FunctionalAnnotation_match <- FunctionalAnnotation[gds_vnt_match_idx, ]
+
+stopifnot(all(FunctionalAnnotation_match$VarInfo == varid_gds))
+
 Anno.folder <- index.gdsn(genofile, "annotation/info")
 # add replace=T to overwrite old annotations?
-add.gdsn(Anno.folder, "FunctionalAnnotation", val=FunctionalAnnotation, compress="LZMA_ra", closezip=TRUE, replace=TRUE)
+add.gdsn(Anno.folder, "FunctionalAnnotation", val=FunctionalAnnotation_match, compress="LZMA_ra", closezip=TRUE, replace=TRUE)
 
 seqClose(genofile)
 
 # remove extra slashes in command so that aws copies to correct location
 # (and doesn't make stupid '/' directoires WITHIN a directory)
-cmd <- paste0("aws s3 cp ", gds.path.local, " ",  gsub("/mnt/", "s3://", gds.path))
+cmd <- paste0("aws s3 mv ", gds.path.local, " ",  gsub("/mnt/", "s3://", gds.path))
 cmd_clean <- gsub("(?<!:)//+", "/", cmd, perl = TRUE)
 system(cmd_clean)
 
