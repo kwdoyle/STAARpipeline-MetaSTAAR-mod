b725e375d170b1026f07c04f9b0ba5aba400c526
Kevin Doyle <kwdoyle1@gmail.com>
2026-05-04 10:57:07 -0400

    converting full GDS to bed without filters

--- Seq2BED_wrapper.sh
+++ Seq2BED_wrapper.sh
@@ -4,18 +4,30 @@ source /efs/garcia/users/kd2630/noncoding_telo/STAAR/STAARpipeline-MetaSTAAR-mod
 # This script sources my env and actually runs the conversion script
 
 chr=$1
-gdsdir=~/noncoding_telo/STAAR/TOPMed_Full_Cohort_grm/
-basedir=~/noncoding_telo/STAAR/TOPMed_Full_Cohort_grm/GRM/
-savedir=${basedir}/beds/
+basedir=/efs/garcia/users/kd2630/noncoding_telo/STAAR/STAARpipeline-MetaSTAAR-mod/
+#basedir=~/noncoding_telo/STAAR/TOPMed_Full_Cohort_grm/GRM/
+
+# --- change these:
+#gdsdir=~/noncoding_telo/STAAR/TOPMed_Full_Cohort_grm/
+gdsdir=/efs/garcia/users/kd2630/noncoding_telo/STAAR/10k_Cohort_rm_telo_qv/
+savedir=${gdsdir}/beds_full/
+
+gdsfl=${gdsdir}/10k_chr_${chr}.gds
+savenm=${savedir}/10k_chr_${chr}.out
+#gdsfl=${gdsdir}/topmed_chr_${chr}.gds
+#savenm=${savedir}/topmed_chr_${chr}.out
+#savedir=${basedir}/beds/
 
 mkdir -p $savedir
 
-gdsfl=${gdsdir}/topmed_chr_${chr}.gds
-savenm=${savedir}/topmed_chr_${chr}.out
 
 echo Processing $gdsfl
 echo Will save to $savenm
 
 
-R CMD BATCH --vanilla "--args --gds.file ${gdsfl} --min.AVGDP 10 --filterCat PASS --min.MAF 0.05 --max.miss 0.05 --removeSNPGDS TRUE --prefix.bed ${savenm}" ${basedir}/Seq2BED_wrapper.R ${savedir}/Seq2BED_wrapper_${chr}.Rout
+# For GDS creation w/ default filters:
+#R CMD BATCH --vanilla "--args --gds.file ${gdsfl} --min.AVGDP 10 --filterCat PASS --min.MAF 0.05 --max.miss 0.05 --removeSNPGDS TRUE --prefix.bed ${savenm}" ${basedir}/Seq2BED_wrapper.R ${savedir}/Seq2BED_wrapper_${chr}.Rout
+
+# For regenie, converting full GDS to bed, no filters:
+R CMD BATCH --vanilla "--args --gds.file ${gdsfl} --min.AVGDP 0 --filterCat PASS --min.MAF 0 --max.miss 0 --removeSNPGDS TRUE --prefix.bed ${savenm}" ${basedir}/Seq2BED_wrapper.R ${savedir}/Seq2BED_wrapper_${chr}.Rout
 
