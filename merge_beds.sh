0b85fe35b410538f127983851526c60ac11eb88a
Kevin Doyle <kwdoyle1@gmail.com>
2026-05-04 10:59:33 -0400

    allow for if prune = true

--- merge_beds.sh
+++ merge_beds.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-saveprefix=topmed_chr_all
-#saveprefix=10k_chr_all
+#saveprefix=topmed_chr_all
+saveprefix=10k_chr_all
 #saveprefix=10k_10th_pcnt_telo_case_chr_all
+
+# Set to true when running GRM pipeline
+prune=False #True
 
 
 for i in {1..22}; do
-	echo topmed_chr_${i}.out >> mergeBED.list
-	#echo 10k_chr_${i}.out >> mergeBED.list
+	#echo topmed_chr_${i}.out >> mergeBED.list
+	echo 10k_chr_${i}.out >> mergeBED.list
 	#echo 10k_10th_pcnt_telo_case_chr_${i}.out >> mergeBED.list
 done
 
@@ -16,11 +19,13 @@ echo Making full bed file
 # ../plink  # originally used plink binary from FastSparseGRM, but should be able to use any plink (in theory)
 plink --merge-list mergeBED.list --make-bed --out $saveprefix
 
-# prune bed file (make lists)
-echo Pruning bed file
-plink  --bfile $saveprefix --indep-pairwise 50 5 0.1 --out ${saveprefix}.prunedlist
+if [[ $prune == True ]]; then
+	# prune bed file (make lists)
+	echo Pruning bed file
+	plink  --bfile $saveprefix --indep-pairwise 50 5 0.1 --out ${saveprefix}.prunedlist
 
-# save pruned bed
-echo Saving pruned bed file
-plink --bfile $saveprefix --extract ${saveprefix}.prunedlist.prune.in --make-bed --out ${saveprefix}_pruned
+	# save pruned bed
+	echo Saving pruned bed file
+	plink --bfile $saveprefix --extract ${saveprefix}.prunedlist.prune.in --make-bed --out ${saveprefix}_pruned
+fi
 
